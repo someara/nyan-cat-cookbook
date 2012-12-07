6ac36e81dcd7467ef2e32777e4284bde97395fe1
Sean OMeara <someara@opscode.com>
2012-12-07 14:17:11 -0500

    bumping metatdata version

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email "someara@opscode.com"
 license          "apache2"
 description      "Installs/Configures nyan-cat"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 
