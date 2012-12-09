080c92068f58250b26a146a681590df89cc46a33
Sean OMeara <someara@opscode.com>
2012-12-09 14:54:10 -0500

    updating to 0.1.3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "someara@opscode.com"
 license          "apache2"
 description      "Installs/Configures nyan-cat"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.2"
+version          "0.1.3"
 
 depends 'line'
 
